82756420a0301a9b66c2d47daea567cc33eb228f
FerMod <tudelaigarcia@gmail.com>
2022-06-05 11:43:31 +0200

    Only allow pick one item with curse of labyrinth

--- start_item_options/main.lua
+++ start_item_options/main.lua
@@ -1,6 +1,12 @@
 local json = require("json")
 local mod = RegisterMod("Start Item Options", 1)
 
+local debug = true
+local function debugPrint(...)
+  if (not debug) then return end
+  print(...)
+end
+
 local spawnPositions = {
   Vector(250, 300),
   Vector(200, 400),
@@ -13,19 +19,23 @@ local data = {
   initialItems = {},
 }
 
-local function dump(object, indentLevel)
+
+local function dump(object, indentLevel, indentStr)
   if type(object) == 'table' then
     indentLevel = indentLevel or 0
-    local indentStr = ''
+    indentStr = indentStr or '  '
+
+    local currentIndentStr = ''
     for i = 1, indentLevel do
-      indentStr = indentStr .. '  '
+      currentIndentStr = currentIndentStr .. indentStr
     end
+
     local s = '{\n'
     for k, v in pairs(object) do
       if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. indentStr .. '[' .. k .. '] = ' .. dump(v, indentLevel + 1) .. ',\n'
+      s = s .. currentIndentStr .. '[' .. k .. '] = ' .. dump(v, indentLevel + 1) .. ',\n'
     end
-    return s .. indentStr .. '}'
+    return s .. '}'
   else
     return tostring(object)
   end
@@ -53,6 +63,11 @@ local function isTreasureRoom()
   return Game():GetLevel():GetCurrentRoom():GetType() == RoomType.ROOM_TREASURE
 end
 
+local function shouldRemoveCollectible()
+  if not isTreasureRoom() then return false end
+  return Game():GetRoom():IsFirstVisit() and data.allowPickAnother
+end
+
 local function isCollectible(entity)
   return entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE
 end
@@ -62,21 +77,101 @@ local function isCurseOfLabyrinth()
   return (curse & LevelCurse.CURSE_OF_LABYRINTH) == LevelCurse.CURSE_OF_LABYRINTH
 end
 
+---Check player has active item in any of the 4 slots.
+---@param playerIndex integer
+---@param itemId integer
+---@return boolean
+local function playerHasActive(playerIndex, itemId)
+  local player = Isaac.GetPlayer(playerIndex)
+  for index = 0, 3 do
+    if player:GetActiveItem(index) == itemId then
+      return true
+    end
+  end
+  return false
+end
+
+---Check player has collectible.
+---@param playerIndex integer
+---@param itemId integer
+---@return boolean
+local function playerHasCollectible(playerIndex, itemId)
+  local player = Isaac.GetPlayer(playerIndex)
+  return player:HasCollectible(itemId)
+end
+
+---Check any player has the item id.
+---@param itemId integer
+---@return boolean
+local function anyPlayerHasItem(itemId)
+  local game = Game()
+  local numPlayers = game:GetNumPlayers()
+  print('numPlayers', numPlayers)
+  for playerIndex = 0, numPlayers - 1 do
+    print('player', playerIndex)
+
+    if playerHasActive(playerIndex, itemId) == itemId then
+      return true
+    end
+
+    if playerHasCollectible(playerIndex, itemId) then
+      return true
+    end
+  end
+
+  return false
+  -- return Isaac.GetPlayer():GetCollectibleNum(itemId, true) > 0;
+end
+
 local function spawnItem(position, optionGroupIndex)
-  local itemPool = Game():GetItemPool()
-  local collectibleID = itemPool:GetCollectible(
+  local game = Game()
+  local itemPool = game:GetItemPool()
+  local collectibleId = itemPool:GetCollectible(
     ItemPoolType.POOL_TREASURE,
-    true,
-    Game():GetRoom():GetSpawnSeed()
+    false,
+    game:GetRoom():GetSpawnSeed()
   )
-  local entity = Isaac.Spawn(
-    EntityType.ENTITY_PICKUP,
-    PickupVariant.PICKUP_COLLECTIBLE,
-    collectibleID, -- 0
-    position,
-    Vector.Zero,
-    nil
-  )
+
+  local spawnEntity = function(id)
+    local subType = id or CollectibleType.COLLECTIBLE_NULL
+    return game:Spawn(
+      EntityType.ENTITY_PICKUP, -- Type
+      PickupVariant.PICKUP_COLLECTIBLE, -- Variant
+      position, -- Position
+      Vector.Zero, -- Velocity
+      nil, -- Parent
+      subType, -- SubType
+      game:GetRoom():GetSpawnSeed()-- Seed (the "GetSpawnSeed()" function gets a reproducible seed based on the room, e.g. "2496979501")
+    )
+    --[[
+    return Isaac.Spawn(
+      EntityType.ENTITY_PICKUP, -- Type
+      PickupVariant.PICKUP_COLLECTIBLE, -- Variant
+      id, -- SubType
+      position, -- Position
+      Vector.Zero, -- Velocity
+      nil-- Spawner
+    )
+    ]]
+  end
+
+  local entity = spawnEntity()
+  --[[   print(entity.SubType)
+  if (anyPlayerHasItem(entity.SubType)) then
+    print('Has item!', entity.SubType)
+    spawnEntity()
+  end ]]
+
+
+  -- local entity = Isaac.Spawn(
+  --   EntityType.ENTITY_PICKUP,
+  --   PickupVariant.PICKUP_COLLECTIBLE,
+  --   collectibleId, -- 0
+  --   position,
+  --   Vector.Zero,
+  --   nil
+  -- )
+
   if (optionGroupIndex) then
     entity:ToPickup().OptionsPickupIndex = optionGroupIndex
   end
@@ -105,7 +200,7 @@ function mod:postNewLevel()
   }
 
   for _, position in ipairs(spawnPositions) do
-    local itemId = spawnItem(position)
+    local itemId = spawnItem(position, 1)
     data.initialItems[itemId] = true
   end
 
@@ -140,11 +235,20 @@ function mod:removeTreasure()
   if (not isNormalRun()) then return end
   if (not isFirstStage()) then return end
   if (not isTreasureRoom()) then return end
+  print('IsFirstVisit', Game():GetRoom():IsFirstVisit())
   if (not Game():GetRoom():IsFirstVisit()) then return end
+  print('allowPickAnother', data.allowPickAnother)
+  if (data.allowPickAnother) then
+    -- Dont let pick other treasure room collectible (if present)
+    data.allowPickAnother = false
+    return
+  end
+
 
   local entities = Isaac.GetRoomEntities()
   for _, entity in ipairs(entities) do
     if isCollectible(entity) then
+      print(entity.SubType)
       entity:Remove()
     end
   end
@@ -169,6 +273,8 @@ function mod:postUpdate()
     return
   end
 
+  --[[
+  -- Remove our spawned collectibles
   local entities = Isaac.GetRoomEntities()
   for _, entity in ipairs(entities) do
     print(entity.Type, entity.Variant, entity.SubType, '|', isCollectible(entity), '|', data.initialItems[entity.SubType])
@@ -176,7 +282,7 @@ function mod:postUpdate()
       entity:Remove()
     end
   end
-
+  ]]
   mod:RemoveCallback(ModCallbacks.MC_POST_UPDATE, mod.postUpdate)
 end
 
